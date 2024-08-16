820ebaf2d9cf0c5627971fa7a66301d83412f586
Яков Хватов <YakovX95@mail.ru>
2024-08-16 04:58:17 +0300

    +Валидация к пагинации

--- TechRequest.API/Parameters/Request/PaginationParams.cs
+++ TechRequest.API/Parameters/Request/PaginationParams.cs
@@ -1,8 +1,13 @@
-﻿namespace TechRequest.API.Parameters.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechRequest.API.Parameters.Request
 {
     public class PaginationParams
     {
+        [Range(1, int.MaxValue, ErrorMessage = "Page must be greater than 0.")]
         public int Page { get; set; } = 1; // Номер страницы по умолчанию
+
+        [Range(1, 100, ErrorMessage = "PageSize must be between 1 and 100.")]
         public int PageSize { get; set; } = 10; // Количество элементов на странице по умолчанию
     }
 }
